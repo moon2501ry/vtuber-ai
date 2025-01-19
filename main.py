f3026b74f60bc752f9f34d3014eae746d2ab1593
Ryan Amorim <159044164+moon2501ry@users.noreply.github.com>
2025-01-19 18:39:26 -0300

    Atualiza .gitignore e implementa integração com pygame para reprodução de áudio na Vtuber AI

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from vtuber_ai import VtuberAI
+import pygame
+from s import c # Descarte
 
-vtuber = VtuberAI(); #Colocar api-key dps
+vtuber = VtuberAI(c.get('API-KEY'));
 # espressões = VtuberAI(); #Expressões da Vtuber
 
-vtuber.conversa.append({"role": "system", "content": "Você é uma Vtuber AI, que faz videos e lives para a Twitch e Youtube. Seu nome é Yamato. Fale em Português a menos que peçam."});
+vtuber.conversa.append({"role": "system", "content": c.get('PROMPT-INITIAL')});
+pygame.mixer.init();
 while True:
     _input = input("Fale com a Vtuber: ");
+    if _input == 'quit':
+        break;
+    pygame.mixer.music.unload();
     vtuber.conversa.append({"role": "user", "content": str(_input)});
     response = vtuber.response();
     vtuber.conversa.append(response);
     print("Yamato:", response.content);
-    # vtuber.text_to_speech(text=response.content);
-    # vtuber.save_audio();
+    vtuber.text_to_speech(voice_id=c.get('GIRL-ID'), text=response.content);
+    vtuber.save_audio();
+    pygame.mixer.music.load('audio.mp3');
+    pygame.mixer.music.play();
